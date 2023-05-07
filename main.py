26125b9ab2566116a89259a848f2188cba842fc3
adam baran <barad@o2.pl>
2023-05-07 08:23:35 +0000

    update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import uvicorn
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 from fastapi import FastAPI
-from pydantic import BaseModel
+# from pydantic import BaseModel
 import yfinance as yf
 
 app = FastAPI()
